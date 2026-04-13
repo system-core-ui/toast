c6ec23c3d7cdf53cad0ea9c8c4724cb2d492a2f8
Quoc Thanh <qthan1004@github.com>
2026-04-13 23:36:57 +0700

    fix(toast): add 'default' to ToastVariant type to resolve typescript storybook error

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-export type ToastVariant = 'success' | 'error' | 'warning' | 'info';
+export type ToastVariant = 'default' | 'success' | 'error' | 'warning' | 'info';
 export type ToastPlacement = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left' | 'top-center' | 'bottom-center';
 
 export interface ToastMessage {
